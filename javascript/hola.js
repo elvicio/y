8a65a45cf79a310109f5b079793cb5258cd316e0
elvicio <juansebastian23982@gmail.com>
2024-08-14 07:57:17 -0500

    Add files via upload

---
+++ javascript/hola.js
@@ -0,0 +1,5 @@
+alert('holamundo')
+alert('jhonatan')
+alert('temu')
+'use script'
+alert(3+2+1)
---
+++ javascript/index.js
@@ -0,0 +1,38 @@
+'use script'
+// variables cons varlet
+// tipos
+//  const texto = '5 +5'todo lo que este entre comillas simples o dobles es un texto'
+const number = 5 + 5
+const booleana = true //true false null
+
+console.log(texto)
+console.log(number)
+console.log(booleana)
+
+const QUeNuncaCambia = 'otra cosa'
+console.log(QueNuncaCambia)
+
+var SiCambia = 'esta si cambia'
+console.log(SiCambia)
+
+SiCambia = 'otra cosa'
+console.log(SiCambia)
+
+if (booleana) {
+    SiCambia ='otra cosa'
+    let SiCambiPeroSoloDentroDe =
+    'Si cambia pero solo existe dentro de la funcion que se'
+    
+    let SiCambiaPeroSoloDentroDe = 
+'si cambia pero solo existe dentro de la funcion que se crea'
+}
+// Incluir una variable dentro de una variable
+const nombre = 'jhonatan'
+var edad = 14
+const sexo = 'mero macho pecho peludo'
+var estadoCivil = 'soltero'
+
+var presentacion = `Hola mi nombre es $
+{nombre} tengo ${edad} años de edad, me identifico como un ${sexo} y mi estado civil es ${estadoCivil} asi que aprovechen´
+
+console.log(presentacion)*/
